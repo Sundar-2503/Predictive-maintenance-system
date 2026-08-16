8c64804ae90ecbf2dafa75aad180f09a212af61a
Sundar-2503 <srinjsundar@gmail.com>
2026-08-16 21:21:51 +0530

    fix-2

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -2,13 +2,13 @@ import sqlite3
 import pandas as pd
 from pathlib import Path
 BASE_DIR=Path(__file__).resolve().parent.parent
-df=pd.read_csv(BASE_DIR/"data"/"ai4i2020.csv")
-connection=sqlite3.connect("database/maintanence.db")
-df.to_sql(
-    "machine_data",
-    connection,
-    if_exists="replace",
-    index=False
-)
-connection.close
-print("DB created")
+DB_PATH=BASE_DIR/"database"/"maintanence.db"
+connection=sqlite3.connect(DB_PATH)
+# df.to_sql(
+#     "machine_data",
+#     connection,
+#     if_exists="replace",
+#     index=False
+# )
+# connection.close
+# print("DB created")
